b7f6bae9bdfcce11fd0a00e926124ede17d3edaa
dasha3412 <100881273+dasha3412@users.noreply.github.com>
2026-09-11 11:04:42 +0800

    Add: longest-common-prefix - submission-0

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+        let l = ""
+        let i = 0
+        while (true) {
+            for (let j = 0; j < strs.length; j++) {
+                console.log(i, j, l)
+                if (!l[i]) {
+                    // set first char
+                    l = l + strs[j][i]
+                }
+                if (l[i] != strs[j][i]) {
+                    return l.slice(0, i)
+                }
+            }
+            i = i + 1
+        }
+
+    }
+}
